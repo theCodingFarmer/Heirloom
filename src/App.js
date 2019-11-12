d916b28f901bcb96e30a1f7a65bc99cd79bc684e
Tye Campbell <tylerjoelcampbell@gmail.com>
2019-11-11 20:37:04 -0600

    added logo and landing page text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 
 function App() {
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Our site is getting a fresh look for 2020! Please reach out to us via <a href="mailto:farmer@hlfarm.com">email</a> or check us out on <a href="https://www.facebook.com/hlfarm/">Facebook</a>. 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
