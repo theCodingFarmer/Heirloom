a641f5f207b907200f89c87cda49c09be8f75537
Tye Campbell <tylerjoelcampbell@gmail.com>
2019-11-12 05:31:33 -0600

    added icons for email and facebook

--- src/App.js
+++ src/App.js
@@ -8,8 +8,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Our site is getting a fresh look for 2020! Please reach out to us via <a href="mailto:farmer@hlfarm.com">email</a> or check us out on <a href="https://www.facebook.com/hlfarm/">Facebook</a>. 
+          Our site is getting a fresh look for our 2020 season! <br/> Please reach out to us via email or check us out on Facebook. 
         </p>
+        <div className="App-icons">
+          <a href="https://www.facebook.com/hlfarm/">
+            <i href="" className="fab fa-facebook"></i>
+          </a>
+          <a href="mailto:farmer@hlfarm.com">
+            <i className="fas fa-envelope"></i>
+          </a>
+        </div>
       </header>
     </div>
   );
