7e81b57f44433b5bce23ba700d91d675d87a3807
Tye Campbell <tcampbell@hy-vee.com>
2021-01-16 11:14:23 -0600

    completely removed the skewed component. will not be using it as designed for now

--- src/components/facts.js
+++ src/components/facts.js
@@ -1,7 +1,45 @@
 import styled from '@emotion/styled';
 import React from 'react';
-import StyledSkewedSection from './skewed-section';
 import { StyledH1 } from './_shared/styled-headings';
+
+const StyledSectionContainer = styled.section`
+  margin: 0 0 6rem 0;
+  position: relative;
+  padding: calc(100% * 0.09719) 0;
+
+  & > .content {
+    max-width: 1000px;
+    margin: 0 auto;
+    padding: 1.5em;
+    position: relative;
+    
+    /* uncomment for debuggind purposes */
+    /* border: 2px dashed #fff8; */
+  }
+
+  &:after {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+    transform-origin: 50% 0;
+    outline: 1px solid transparent;
+    backface-visibility: hidden;
+    background-color: var(--bg-content-color);
+  }
+`;
+
+const StyledContentWrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-direction: column;
+  margin: 0 auto;
+  width: 95%;
+`;
 
 const StyledLogoSection = styled.section`
   width: 100%;
@@ -44,23 +82,25 @@ const StyledFactNumber = styled.section`
 
 const Facts = () => {
   return (
-    <StyledSkewedSection angle={10}>
-      <StyledH1>Some Interesting Facts</StyledH1>
-      <StyledLogoSection>
-        <StyledFactNumber>
-          <span className="number">127</span>
-          <span className="fact">Finished Projects</span>
-        </StyledFactNumber>
-        <StyledFactNumber>
-          <span className="number">321</span>
-          <span className="fact">Happy Customers</span>
-        </StyledFactNumber>
-        <StyledFactNumber>
-          <span className="number">863</span>
-          <span className="fact">Freelancing Hours</span>
-        </StyledFactNumber>
-      </StyledLogoSection>
-    </StyledSkewedSection>
+    <StyledSectionContainer>
+      <StyledContentWrapper>
+        <StyledH1>Some Interesting Facts</StyledH1>
+        <StyledLogoSection>
+          <StyledFactNumber>
+            <span className="number">127</span>
+            <span className="fact">Finished Projects</span>
+          </StyledFactNumber>
+          <StyledFactNumber>
+            <span className="number">321</span>
+            <span className="fact">Happy Customers</span>
+          </StyledFactNumber>
+          <StyledFactNumber>
+            <span className="number">863</span>
+            <span className="fact">Freelancing Hours</span>
+          </StyledFactNumber>
+        </StyledLogoSection>
+      </StyledContentWrapper>
+    </StyledSectionContainer>
   );
 };
 
