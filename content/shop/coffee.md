053a24d380a14f36a03c039343dc317fd81b1c45
Tye Campbell <tcampbell@hy-vee.com>
2021-03-30 20:21:39 -0500

    working on graphql query to display

---
+++ content/shop/coffee.md
@@ -0,0 +1,10 @@
+---
+available: true  
+date: '2021-02-07'  
+title: 'Bearded Brothers Coffee'  
+cover_image: './images/coffee.jpg'  
+price: 14  
+options: ['Pre Order']
+---
+
+This is the best locally-roasted coffee we can find! Each variety comes from a single source; they don’t mix in cheap beans. So when you buy their Sumatran, Brazilian, Kenyan, or Bali, for example, each bag only contains beans from that country. The result is a high-quality coffee that you’ll love.
--- src/pages/shop.js
+++ src/pages/shop.js
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import { graphql, Link } from 'gatsby';
+import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
 import Layout from '../components/layout';
@@ -9,16 +9,13 @@ import { blogMenuLinks } from '../components/_config/menu-links';
 import { StyledH1 } from '../components/_shared/styled-headings';
 import { StyledFullHeightSection } from '../components/_shared/styled-section';
 import { StyledSeparator } from '../components/_shared/styled-separator';
+import ShopProducts from '../components/ShopProducts';
 
 const StyledTagsH1 = styled(StyledH1)`
   margin-top: 3rem;
 `;
 
-const Shop = ({
-  data: {
-    allMarkdownRemark: { group },
-  },
-}) => {
+const Shop = ({data}) => {
 
   return (
     <Layout menuLinks={blogMenuLinks}>
@@ -26,6 +23,7 @@ const Shop = ({
       <StyledFullHeightSection>
         <StyledTagsH1>Shop</StyledTagsH1>
         <StyledSeparator />
+        <ShopProducts available={data.products.nodes}/>
         This is where you can buy stuff.
         <StyledSeparator />
         <TextLink label="Take me home" link="/" />
@@ -53,14 +51,28 @@ Shop.propTypes = {
 };
 export default Shop;
 
-export const pageQuery = graphql`
-  query {
-    allMarkdownRemark(
-      filter: { fileAbsolutePath: { regex: "/content/posts/" }, frontmatter: { published: { eq: true } } }
+export const shopQuery = graphql`
+  {
+    products: allMarkdownRemark(
+      limit: 3
+      sort: { order: DESC, fields: frontmatter___date }
+      filter: { fileAbsolutePath: { regex: "/content/shop/" }, frontmatter: { available: { eq: true } } }
     ) {
-      group(field: frontmatter___tags) {
-        fieldValue
-        totalCount
+      nodes {
+        frontmatter {
+          date(formatString: "D MMMM, YYYY")
+          title
+          price
+          options
+          cover_image {
+            childImageSharp {
+              fluid(maxWidth: 800) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+        html
       }
     }
   }
